9ee328698bd83aa7eb2b83e7941f7399ced78501
최정윤 <janet971015@naver.com>
2024-05-27 14:15:41 +0900

    init

---
+++ OnJeong_BE/src/test/java/com/a503/onjeong/OnjeongApplicationTests.java
@@ -0,0 +1,12 @@
+package com.a503.onjeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class OnjeongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
